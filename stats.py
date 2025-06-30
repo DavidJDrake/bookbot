ca953f7b888873d4a8a07da544b5bf0ed68b4dda
jaydrake <fatherofash@gmail.com>
2025-06-30 16:59:26 -0400

    Fixing looping logic for count_characters

--- stats.py
+++ stats.py
@@ -7,9 +7,14 @@ def count_characters(text):
     for char in text:
         if char.isalpha():
             char = char.lower()
-            if char in character_list:
-                pass
-            else:
-                pass
+            for character in character_list:
+                if character['name'] == char:
+                    print(f"Character '{char}' already exists in the list.")
+                    character['num'] += 1
+                else:
+                    print(f"Adding character '{char}' to the list.")
+                    character_dict = {"name": char, "num": 1}
+                    character_list.append(character_dict)
     character_list.sort()
+    print(character_list)
     return(character_list)
