298b12bcce9570d7cd6061cc1e274e1abaffdc57
jaydrake <fatherofash@gmail.com>
2025-07-02 14:01:16 -0400

    Completed lesson 13/15

--- stats.py
+++ stats.py
@@ -8,22 +8,22 @@ def count_characters(text):
         if char.isalpha():
             char = char.lower()
             if not character_list:
-                print(f"Adding first character: {char}")
+                #print(f"Adding first character: {char}")
                 new_character = {"name": char, "num": 1}
                 character_list.append(new_character)
             else:
                 found = False
                 for character in character_list:
                     if character["name"] == char:
-                        print(f"Found existing character: {char}")
+                        #print(f"Found existing character: {char}")
                         character['num'] += 1
                         found = True
                         break
                 if not found:
-                    print(f"Adding new character: {char}")
+                    #print(f"Adding new character: {char}")
                     new_character = {"name": char, "num": 1}
                     character_list.append(new_character)
 
-    #character_list.sort()
-    print(character_list)
+    character_list.sort(reverse=True, key=lambda x: x['num'])
+    #print(character_list)
     return(character_list)
